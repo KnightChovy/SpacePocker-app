07e880b21cb8c70ecfe191d7d9c410abc3a1d333
nguyenkimthienngan <ngannktsa180013@fpt.edu.vn>
2026-01-21 14:55:50 +0700

    Create Login page

--- client/src/pages/page-example.tsx
+++ client/src/pages/page-example.tsx
@@ -1,9 +1,14 @@
+import { Button } from '@/components/ui/button';
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
-const page = () => {
+const Page = () => {
+  const navigate = useNavigate();
   return (
-    <div>page-example</div>
+    <div>page-example
+      <Button onClick={() => navigate('/auth-login')}>Login</Button>
+    </div>
   )
 }
 
-export default page
+export default Page
