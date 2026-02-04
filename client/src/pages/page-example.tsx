21359915fe62609fec1b400bfa9c15d3dce2da35
ptphat-vn <tanphatphan091@gmail.com>
2026-02-04 14:31:47 +0700

    chore(client): add setup commitlint husky

--- client/src/pages/page-example.tsx
+++ client/src/pages/page-example.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 const page = () => {
-  return (
-    <div>page-example</div>
-  )
-}
+  return <div>page-example</div>;
+};
 
-export default page
+export default page;
