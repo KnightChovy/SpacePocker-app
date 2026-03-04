2c66e027e4855ac2276d21133b3007b2d382b3ab
TanMinhNgo <tannmse182434@fpt.edu.vn>
2026-03-04 14:09:30 +0700

    fix: fix routing

--- client/src/routes/adminRoute.tsx
+++ client/src/routes/adminRoute.tsx
@@ -4,18 +4,21 @@ import SpacesPage from '@/pages/admin/SpacesPage';
 import UsersPage from '@/pages/admin/UsersPage';
 import SettingsPage from '@/pages/admin/SettingsPage';
 import SecurityPage from '@/pages/admin/SecurityPage';
+import AdminLayout from '@/components/layouts/AdminLayout';
 import { Routes, Route, Navigate } from 'react-router-dom';
 
 const AdminRoute = () => {
   return (
     <Routes>
-      <Route path="/dashboard" element={<DashboardPage />} />
-      <Route path="/users" element={<UsersPage />} />
-      <Route path="/spaces" element={<SpacesPage />} />
-      <Route path="/finance" element={<FinancialPage />} />
-      <Route path="/settings" element={<SettingsPage />} />
-      <Route path="/security" element={<SecurityPage />} />
-      <Route path="*" element={<Navigate to="/admin/dashboard" replace />} />
+      <Route element={<AdminLayout />}>
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/users" element={<UsersPage />} />
+        <Route path="/spaces" element={<SpacesPage />} />
+        <Route path="/finance" element={<FinancialPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/security" element={<SecurityPage />} />
+        <Route path="*" element={<Navigate to="/admin/dashboard" replace />} />
+      </Route>
     </Routes>
   );
 };
