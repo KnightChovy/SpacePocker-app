59fab58fc987a42b7b22c8c9365aebacde75b7a7
TanMinhNgo <tannmse182434@fpt.edu.vn>
2026-03-10 10:30:37 +0700

    feat: add gluestack ui

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,6 +1,9 @@
 import { Stack } from "expo-router";
 import './global.css';
 
+import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
+import '@/app/global.css';
+
 export default function RootLayout() {
   return <Stack />;
 }
