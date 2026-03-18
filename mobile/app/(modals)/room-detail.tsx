77ac6f2732ce9477cba1b7328bcd0296d49fd33e
ptphat-vn <tanphatphan091@gmail.com>
2026-03-18 14:06:01 +0700

    hotfix: fix logout navigate login screen

---
+++ mobile/app/(modals)/room-detail.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function RoomDetail() {
+  return (
+    <View>
+      <Text>RoomDetail</Text>
+    </View>
+  );
+}
